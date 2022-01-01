fb1b24ae7a7df710790afeb43a25a46bcd2b1047
FranciscoAlex <franciscoalexandrem@gmail.com>
2022-01-01 17:17:34 +0100

    how to use and modify

--- app.py
+++ app.py
@@ -10,32 +10,43 @@ from sklearn.metrics import accuracy_score,confusion_matrix
 import pickle
 import joblib
 
+#----Titulo---> O o codigo em estado de comentario é relativo ao treinamento dos modelos, remove os "#" caso queira treinar com outra dataset ou mudar o modelo
+
+
+#-------> Preparacao do dataset a ser usado
 # data = pd.read_csv("nomes.csv")
 # data.drop("group_name",axis=1, inplace=True)
 # data['frequency_female'] = data['frequency_female'].fillna(0)
 # data['frequency_male'] = data['frequency_male'].fillna(0)
 # x_train,x_test,y_train,y_test = train_test_split(data['alternative_names'],data['classification'],test_size=0.25,random_state=7,shuffle=True)
 
+#-------> Transformando todos os dados na tabela em um vector by* demencional
 # tfidf_vectorizer = TfidfVectorizer(stop_words='english',max_df=0.75)
 
 vectorizer = joblib.load("./trainedModels/tfidf_vectorizer.pickle")
 
+#-------> Transformando os dados tipo string em int.
 # vec_train = tfidf_vectorizer.fit_transform(x_train.values.astype('U'))
 # vec_test = tfidf_vectorizer.transform(x_test.values.astype('U'))
+#-------> Salvar o classificador
 # pickle.dump(tfidf_vectorizer, open("./trainedModels/tfidf_vectorizer.pickle", "wb"))
 
+#-------> Treinando o modelo com os dados já transformados e salvando eles
 # pac = PassiveAggressiveClassifier(max_iter=50)
 vectorizerPac = joblib.load("./trainedModels/passiveAgressive.pickle")
 # pac.fit(vec_train,y_train)
 # pickle.dump(pac, open("./trainedModels/passiveAgressive.pickle", "wb"))
 
+#-------> A acuracia actual 
 # 70.53%
 
+#-------> calculando a acuracia em porcentagens
 # y_pred = pac.predict(vec_test)
 # score = accuracy_score(y_test,y_pred)
 
 # print(f'Pac Accuracy :{round(score*100,2)}%')
 
+#-------> funcao INPUT para classificar um nome
 def findlabel(newtext):
     vec_newstest = vectorizer.transform([newtext])
     y_pred1 = vectorizerPac.predict(vec_newstest)
